bd96f96bf75c576c4ddf32c065846ea05bf12fed
Adama2701 <adama_camara@hotmail.dk>
2017-02-15 13:42:44 +0100

    kat

---
+++ myapplication/src/main/java/com/example/myapplication/Logic.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+/**
+ * Created by adama on 15-02-2017.
+ */
+
+public class Logic {
+    Test test = new Test(3, true, "Adama");
+}
---
+++ myapplication/src/main/java/com/example/myapplication/Test.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+/**
+ * Created by adama on 15-02-2017.
+ */
+
+public class Test {
+
+    private int id;
+    private int length;
+    private boolean guess;
+    private String name;
+
+
+    Test(int length, boolean guess, String name){
+        this.length = length;
+        this.guess = guess;
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public boolean getGuess(){
+        return guess;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
